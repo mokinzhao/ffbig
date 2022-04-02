12123a7680264ba9c9749ea0ff9046c659131db2
mokinzhao <zhaoyl3@yonghui.cn>
2022-04-03 00:21:10 +0800

    update

--- code/js/test.js
+++ code/js/test.js
@@ -1,43 +1,25 @@
-class LRUCache{
-    constructor(capacity){
-        this.capacity=capacity
-        this.map=new Map()
-    }
 
-    get(key){
-        if(this.map.has(key)){
-            const temp=this.map.get(key)
-            this.map.delete(key)
-            this.map.set(temp)
-            return temp
+
+
+//10个人， 围成一个环， 报3，从1 开始报
+
+
+//params 人数,报的编号, 
+
+//return 剩下人的 编号
+
+function search(nums,targetNum){
+    if(nums.length<2) return nums
+    let map=new Map()
+    function dfs(){
+        for(let i=0;i<nums.length;i++){
+            if(i==targetNum){
+                nums.splice(0,i)
+                dfs()
+                map.set(nums[i],i)
+            }
         }
-        return -1
-
     }
-
-    set(key,val){
-        if(this.map.has(key)) this.map.delete(key)
-
-        else if  (this.map.size>=this.capacity) {
-            this.map.delete(this.map.keys().next().value)
-        }
-        this.map.set(key,val)
-    }
-    clear(){
-        this.map.clear()
-    }
+    dfs()
+    return [...nums]
 }
-
-
-const cache= new LRUCache(2)
-cache.set('1',1)
-cache.set('2',2)
-console.log(cache.get('1'))
-cache.set('3',3)
-console.log(cache.get('2'))
-cache.set('4',4)
-console.log(cache.get('1'))
-console.log(cache.get('3'))
-console.log(cache.get('4'))
-cache.clear()
-console.log(cache.get('4'))
